c201874307924336bdcd02c316c8980f23f4f977
murata2220 <y.murata.38@future.co.jp>
2019-06-07 18:33:25 +0900

    feat:add gce

---
+++ gce/main.sh
@@ -0,0 +1,29 @@
+#!bin/sh
+if [ -n "$1" ] ; then
+  GCP_PROJECT=$1
+fi
+echo Target project is [$GCP_PROJECT]
+
+delete_cluster () {
+  echo .
+  echo ..
+  echo target is detected...
+  echo target is [$1] in [$2]
+  echo .
+  echo ..
+  echo start deleting...
+  echo .
+  gcloud -q compute instances delete $1 --zone $2 --project ${GCP_PROJECT}
+  echo .
+  echo finished.
+}
+
+gcloud compute instances list --project ${GCP_PROJECT} | awk '$1 != "NAME" {printf "__name__:%s,__zone__:%s\n",$1,$2}' | while read line
+do
+  if [ "`echo $line | grep __name__: `" ]; then
+    NAME=`echo $line | cut -d ',' -f 1 | sed -e 's/__name__://g'`
+    ZONE=`echo $line | cut -d ',' -f 2 | sed -e 's/__zone__://g'`
+    delete_cluster $NAME $ZONE
+  fi
+done
+echo Your project is clean!!
