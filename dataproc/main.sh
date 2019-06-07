4764d47f8fc88d302d94332e3d62712ce3d6fd45
murata2220 <y.murata.38@future.co.jp>
2019-06-07 18:20:19 +0900

    feat:add dataproc

---
+++ dataproc/main.sh
@@ -0,0 +1,28 @@
+#!bin/sh
+if [ -n "$1" ] ; then
+  GCP_PROJECT=$1
+fi
+echo Target project is [$GCP_PROJECT]
+
+delete_cluster () {
+  echo .
+  echo ..
+  echo target cluster is detected...
+  echo target cluster is [$1]
+  echo .
+  echo ..
+  echo start deleting...
+  echo .
+  gcloud -q dataproc clusters delete $1 --project ${GCP_PROJECT}
+  echo .
+  echo finished.
+}
+
+gcloud dataproc clusters list --project ${GCP_PROJECT} | awk '$1 != "NAME" {printf "__name__:%s\n",$1}' | while read line
+do
+  if [ "`echo $line | grep __name__: `" ]; then
+    NAME=`echo $line | sed -e 's/__name__://g'`
+    delete_cluster $NAME
+  fi
+done
+echo Your project is clean!!
