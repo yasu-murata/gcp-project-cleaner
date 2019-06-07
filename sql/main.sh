19e43e795a0699512e994845eed523b7a32273bf
murata2220 <y.murata.38@future.co.jp>
2019-06-07 21:23:35 +0900

    feat:add sql

---
+++ sql/main.sh
@@ -0,0 +1,28 @@
+#!bin/sh
+if [ -n "$1" ] ; then
+  GCP_PROJECT=$1
+fi
+echo Target project is [$GCP_PROJECT]
+
+destroy () {
+  echo .
+  echo ..
+  echo target is detected...
+  echo target is [$1]
+  echo .
+  echo ..
+  echo start deleting...
+  echo .
+  gcloud -q sql instances delete $1 --project ${GCP_PROJECT}
+  echo .
+  echo finished.
+}
+
+gcloud sql instances list --project ${GCP_PROJECT} | awk '$1 != "NAME" {printf "__name__:%s\n",$1}' | while read line
+do
+  if [ "`echo $line | grep __name__: `" ]; then
+    NAME=`echo $line | sed -e 's/__name__://g'`
+    destroy $NAME
+  fi
+done
+echo Your project is clean!!
